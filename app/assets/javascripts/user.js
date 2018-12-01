da28b26c12cbe5fd660c3c74c7f13e6523a12ed3
kickthekaz <kick.the.kaz@gmail.com>
2018-12-01 17:05:04 +0900

    実装完了

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,9 +1,10 @@
 $(function() {
-
+ console.log('aaa')
   var search_user = $("#user-search-result");
+  var add_user = $("#user-add-result");
 
   function appendUsers(users) {
-    var html = `<div class="chat-group-user clearfix">
+    var html = `<div class="chat-group-user clearfix" id="appendUsers">
                   <p class="chat-group-user__name">${users.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${users.id}" data-user-name="${users.name}">追加</a>
                 </div>`
@@ -17,8 +18,18 @@ $(function() {
     search_user.append(html);
   }
 
+  function appendaddUser(user) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value="${user.id}">
+                  <p class='chat-group-user__name'>${user.name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    add_user.append(html);
+  }
+
   $("#user_ids").on("keyup", function() {
     var input = $("#user_ids").val();
+    console.log(input)
     $.ajax({
       type: 'GET',
       url: '/users',
@@ -30,6 +41,19 @@ $(function() {
       if(users.length !== 0){
         users.forEach(function(user){
           appendUsers(user);
+            $(document).on("click","#appendUsers", function(users) {
+              $("#appendUsers").empty();
+                appendaddUser(user);
+            });
+            // $(document).on("submit","#chat-group-user-8",function() {
+            //     var user_ids = $("#chat-group-user-8").val();
+            //     $.ajax({
+            //       type: 'POST',
+            //       url: '/groups/:id',
+            //       data: { user_ids },
+            //       dataType: 'json'
+            //     })
+            // });
         });
       }
       else{
@@ -39,6 +63,10 @@ $(function() {
 
     .fail(function() {
       alert('検索に失敗しました');
-    })
+    });
+
+    $(document).on("click","#chat-group-user-8", function() {
+      $("#chat-group-user-8").empty();
+    });
   });
 });
